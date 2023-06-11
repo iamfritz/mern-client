a95e535cae4aa687dc46b4f422e4ce3fe6dce4a7
fritz-darryl <link4anything@yahoo.com>
2023-06-12 02:25:32 +0800

    react crud for expressjs web

---
+++ src/components/recordList.js
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+var api_url = process.env.REACT_APP_API_URL;
+var api_key = process.env.REACT_APP_API_KEY;
+
+const Record = (props) => (
+  <tr>
+    <td>{props.record.name}</td>
+    <td>{props.record.age}</td>
+    <td>{props.record.position}</td>
+    <td>{props.record.level}</td>
+    <td>
+      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>
+        Edit
+      </Link>{" "}
+      |
+      <button
+        className="btn btn-link"
+        onClick={() => {
+          props.deleteRecord(props.record._id);
+        }}
+      >
+        Delete
+      </button>      
+    </td>
+  </tr>
+);
+
+export default function RecordList() {
+  const [records, setRecords] = useState([]);
+    
+  useEffect(() => {
+    async function getRecords() {
+
+        try {
+            //const response = await fetch(`${process.env.API_URL}/record/all`);
+            let url = `${api_url}user/all`;
+            const response = await fetch(url, {
+              method: "GET",
+              Accept: "application/json",
+              headers: {
+                "api-key": api_key
+              },
+            });
+            console.log(response);
+            
+            if (!response.ok) {
+                const message = '';//`An error occurred: ${response.statusText}`;
+                console.log(response);
+                window.alert(message);
+                return;
+            }
+
+            const records = await response.json();
+            setRecords(records);            
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+    }
+
+    getRecords();
+
+    return;
+  }, [records.length]);
+
+  // This method will delete a record
+  async function deleteRecord(id) {
+
+    if (!window.confirm("Are you sure you want to delete the record?")) {
+        return false;
+    }
+
+    try {
+        
+        let url = `${api_url}user/delete/${id}`;
+        const response = await fetch(url, {
+            method: "DELETE",
+            Accept: "application/json",
+            headers: {
+                "api-key": api_key,
+            },
+        });
+
+        console.log(response);
+        if (!response.ok) {
+            const message = "An error has occurred"; //`An error occurred: ${response.statusText}`;
+            console.log(response);
+            window.alert(message);
+            return;
+        }
+        const newRecords = records.filter((el) => el._id !== id);
+        setRecords(newRecords);        
+    } catch (error) {
+        console.log("Error: ", error);
+    }    
+    
+  }
+
+  // This method will map out the records on the table
+  function recordList() {
+    return records.map((record) => {
+      return (
+        <Record
+          record={record}
+          deleteRecord={() => deleteRecord(record._id)}
+          key={record._id}
+        />
+      );
+    });
+  }
+
+  // This following section will display the table with the records of individuals.
+  return (
+    <div>
+      <h3>Record List</h3>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Position</th>
+            <th>Level</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{recordList()}</tbody>
+      </table>
+    </div>
+  );
+}
